2b5005810b1d71cf2814c7d5cf9ad141e8c7b63d
kyletser <106010272+kyletser@users.noreply.github.com>
2023-09-24 14:02:31 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,9 +20,9 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=26501400&pid=13d165b9070c534e1c90784d539a0a09&tid=d5b0647a2dad41f9c9f8ae292856d9bd",
-    "https://www.maimemo.com/share/page?uid=26501400&pid=13d165b9070c534e1c90784d539a0a09&tid=d5b0647a2dad41f9c9f8ae292856d9bd",
-    "hhttps://www.maimemo.com/share/page?uid=26501400&pid=13d165b9070c534e1c90784d539a0a09&tid=d5b0647a2dad41f9c9f8ae292856d9bd",
+    "https://www.maimemo.com/share/page?uid=26501400&pid=f6a29b3caceb2ac456c1391a13c57b1e&tid=57fc7576a73d491eb99c50ce75f8ac4f",
+    "https://www.maimemo.com/share/page?uid=26501400&pid=f6a29b3caceb2ac456c1391a13c57b1e&tid=57fc7576a73d491eb99c50ce75f8ac4f",
+    "https://www.maimemo.com/share/page?uid=26501400&pid=f6a29b3caceb2ac456c1391a13c57b1e&tid=57fc7576a73d491eb99c50ce75f8ac4f",
   ]
   /**
    * 生成随机数字
